b1f97d0c2306bc7db5b40c4812eb41ea8d7a8954
Nauffal Nur Rizky <nauffal2005@gmail.com>
2023-08-02 13:49:08 +0700

    Second Commit

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const chara = document.querySelector("#player");
-const obstacle = document.querySelector("#obstacle");
+const chara = document.querySelector('#player');
+const obstacle = document.querySelector('#obstacle');
+const startBtn = document.querySelector('.start-btn');
+startBtn.addEventListener('click', () => {
+  obstacle.classList.add('obstacle');
+});
+window.addEventListener('click', () => {
+  if (obstacle.classList == 'obstacle') {
+    if (chara.classList != 'jump') {
+      chara.classList.add('jump');
+    }
+    setTimeout(() => {
+      chara.classList.remove('jump');
+    }, 500);
 
-window.addEventListener("click", () => {
-	if (chara.classList != "jump") {
-		chara.classList.add("jump");
-	}
-	setTimeout(() => {
-		chara.classList.remove("jump");
-	}, 500);
+    console.log(`You did it!!!`);
+  }
 });
 
 const checkDead = setInterval(() => {
-	const charaBottom = parseInt(
-		window.getComputedStyle(chara).getPropertyValue("bottom")
-	);
-	const obstacleRight = parseInt(
-		window.getComputedStyle(chara).getPropertyValue("right")
-	);
+  const charaBottom = parseInt(window.getComputedStyle(chara).getPropertyValue('bottom'));
+  const obstacleRight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('right'));
 
-	// console.log(obstacleRight);
-	// if (obstacleRight < 0) {
-	// 	obstacle.style.animation = "none";
-	// 	obstacle.style.display = "none";
-	// 	alert(`YOU LOSE!!!`);
-	// }
+  if (obstacleRight > 942 && charaBottom < 40) {
+    obstacle.classList.remove('obstacle');
+    alert(`YOU LOSE!!!`);
+  }
 }, 10);
